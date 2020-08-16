798c12f1673cb991e9e58cfe991d8770202a1e31
Vincent Rialland <vincent.rialland@gmail.com>
2020-08-16 12:31:10 +0200

    Add project layout

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="resume-builder",
+    version="0.0.1",
+    author="Vincent Rialland",
+    author_email="Vincent Rialland",
+    description="A simple resume builder from YAML data and HTML template",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vrialland/resume-builder",
+    packages=setuptools.find_packages(),
+    install_requires=["jinja2", "starlette", "pyyaml",],
+    extras_require={"tests": ["black", "pytest",]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
